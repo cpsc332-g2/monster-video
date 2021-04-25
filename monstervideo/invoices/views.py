20f06cc82e5e6cee982c629ee1b602a174aa4011
kicacho01 <kicacho@csu.fullerton.edu>
2021-04-25 14:49:56 -0700

    views-update

--- monstervideo/invoices/views.py
+++ monstervideo/invoices/views.py
@@ -47,29 +47,31 @@ def add_appraisal(request, customer_id):
     else:
         return redirect(f"/invoices/{customer_id}")
 
+#Note: The redirect stays the same for each entity
+# customer_id has to be passed to each function below
+
 #May need to edit the parameters and code for each defined functions below
 #appraisal is FK of Proposal
-def add_proposal(request):
+def add_proposal(request, customer_id):
     if request.method=="POST":
-        #appraisal = get_object_or_404(Appraisal, pk=appraisal_id)
+        appraisal = get_object_or_404(Customer, pk=customer_id)     #Unsure
         proposal = Proposal()
-        #proposal.appraisal = appraisal
+        proposal.appraisal = appraisal
         proposal.date = request.POST.get('date')
         proposal.description = request.POST.get('description')
         proposal.save()
-        #Need to edit return statements
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
     else:
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
 
 #proposal and customer are FK of installation 
-def add_installation(request):
+def add_installation(request, customer_id):
     if request.method=="POST":
-        #proposal = get_object_or_404(Proposal, pk=proposal_id)
-        #customer = get_object_or_404(Customer, pk=customer_id)
+        proposal = get_object_or_404(Customer, pk=customer_id) #unsure
+        customer = get_object_or_404(Customer, pk=customer_id)  #unsure
         installation = Installation()
-        #installation.proposal = proposal
-        #installation.customer = customer
+        installation.proposal = proposal
+        installation.customer = customer
         installation.date_start = request.POST.get('date_start')
         installation.date_finish = request.POST.get('date_finish')
         installation.cleanup = request.POST.get('cleanup')
@@ -78,31 +80,29 @@ def add_installation(request):
         installation.total_cost = request.POST.get('total_cost')
         installation.cables_cost = request.POST.get('cables_cost')
         installation.save()
-        #Need to edit return statements
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
     else:
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
  
 #installation is FK of Employee 
-def add_employee(request):
+def add_employee(request, customer_id):
     if request.method=="POST":
-        #installation = get_object_or_404(Installation, pk=installation_id)
+        installation = get_object_or_404(Customer, pk=customer_id)  #unsure
         employee = Employee()
-        #employee.installation = installation
+        employee.installation = installation
         employee.name = request.POST.get('name')
         employee.department = request.POST.get('department')
         employee.save()
-        #need to edit return statements
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
     else:
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
 
 #installation is the FK for Item 
-def add_item(request):
+def add_item(request, customer_id):
     if request.method=="POST":
-        #installation = get_object_or_404(Installation, pk=installation_id)
+        installation = get_object_or_404(Customer, pk=customer_id)  #unsure
         item = Item()
-        #item.installation = installation
+        item.installation = installation
         item.item_description = request.POST.get('item_description')
         item.item_location = request.POST.get('item_location')
         item.item_name = request.POST.get('item_name')
@@ -110,54 +110,58 @@ def add_item(request):
         item.item_labor_cost = request.POST.get('item_labor_cost')
         item.item_cost = request.POST.get('item_cost')
         item.save()
-        #Need to edit return statements
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
     else:
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
  
 #Speaker, AVComponents, and elctronics inherit attributes from class item (Polymorphism/Inheritance)
 #How to implement?
 
-#def add_speakers(request):
-#    if request.method=="POST":
-#        speakers = Speakers()
-#        speakers.total = request.POST.get('total')
-#        speakers.save()
-#        return redirect('/invoices')
-#    else:
-#        return redirect('/invoices')
+def add_speakers(request, customer_id):
+    if request.method=="POST":
+        installation = get_object_or_404(Customer, pk=customer_id)  #unsure
+        speakers = Speakers()
+        speaker.installation = installation
+        speakers.total = request.POST.get('total')
+        speakers.save()
+        return redirect(f"/invoices/{customer_id}")
+    else:
+        return redirect(f"/invoices/{customer_id}")
  
-#def add_avcomponents(request):
-#    if request.method=="POST":
-#        avcomponents = AVComponents()
-#        avcomponents.total = request.POST.get('total')
-#        avcomponents.save()
-#        return redirect('/invoices')
-#    else:
-#       return redirect('/invoices')
+def add_avcomponents(request, customer_id):
+    if request.method=="POST":
+        installation = get_object_or_404(Customer, pk=customer_id)  #unsure
+        avcomponents = AVComponents()
+        avcomponents.installation = installation
+        avcomponents.total = request.POST.get('total')
+        avcomponents.save()
+        return redirect(f"/invoices/{customer_id}")
+    else:
+       return redirect(f"/invoices/{customer_id}")
  
-#def add_electronics(request):
-#    if request.method=="POST":
-#        electronics = Electronics()
-#        electronics.total = request.POST.get('total')
-#        electronics.save()
-#        return redirect('/invoices')
-#    else:
-#        return redirect('/invoices')
+def add_electronics(request, customer_id):
+    if request.method=="POST":
+        installation = get_object_or_404(Customer, pk=customer_id)  #unsure
+        electronics = Electronics()
+        electronics.installation = installation
+        electronics.total = request.POST.get('total')
+        electronics.save()
+        return redirect(f"/invoices/{customer_id}")
+    else:
+        return redirect(f"/invoices/{customer_id}")
 
 #item is FK of Warranty 
-def add_warranty(request):
+def add_warranty(request, customer_id):
     if request.method=="POST":
-        #item = get_object_or_404(Item, pk=item_id)
+        item = get_object_or_404(Customer, pk=customer_id)  #unsure
         warranty = Warranty()
-        #warranty.item = item
+        warranty.item = item
         warranty.start_date = request.POST.get('start_date')
         warranty.title = request.POST.get('title')
         warranty.mfg_warranty = request.POST.get('mfg_warranty')
         warranty.add_warranty = request.POST.get('add_warranty')
         warranty.cost = request.POST.get('cost')
         warranty.save()
-        #Need to edit return statements
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
     else:
-        return redirect('/invoices')
+        return redirect(f"/invoices/{customer_id}")
