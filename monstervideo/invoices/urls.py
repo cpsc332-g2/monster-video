d1ba2353b630723cd086c87979ca215e6d679e8d
Carson <carson.carpenter7@csu.fullerton.edu>
2021-04-26 20:35:42 -0700

    First push

--- monstervideo/invoices/urls.py
+++ monstervideo/invoices/urls.py
@@ -7,4 +7,16 @@ urlpatterns = [
     path('<int:customer_id>/', views.details, name='details'),
     path('add_customer/', views.add_customer, name='add_customer'),
     path('<int:customer_id>/add_appraisal/', views.add_appraisal, name='add_appraisal'),
+    #To-do
+    path('<int:customer_id>/add_proposal/', views.add_proposal, name='add_proposal'),
+    path('<int:customer_id>/add_installation/', views.add_installation, name='add_installation'),
+    path('<int:customer_id>/add_employee/', views.add_employee, name='add_employee'),
+    path('<int:customer_id>/add_item/', views.add_item, name='add_item'),
+    path('<int:customer_id>/add_speaker/', views.add_speaker, name='add_speaker'),
+    path('<int:customer_id>/add_avcomponents/', views.add_avcomponents, name='add_avcomponents'),
+    path('<int:customer_id>/add_electronics/', views.add_electronics, name='add_electronics'),
+    path('<int:customer_id>/add_warranty/', views.add_warranty, name='add_warranty'),
+
+    
+
 ]
