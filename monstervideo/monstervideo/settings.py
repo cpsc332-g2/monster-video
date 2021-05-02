406bee3b9a330d6de5c3a399fc339c2cf2c230fa
David Bonnaud <david@davidbonnaud.com>
2021-05-01 18:50:27 -0700

    minor settings tweaks

--- monstervideo/monstervideo/settings.py
+++ monstervideo/monstervideo/settings.py
@@ -136,8 +136,9 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
-]
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
